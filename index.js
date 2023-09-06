fdac4bcf4a34bf7d87527b2f8a07eb567479af38
Tony Vu <tony.vuu18@gmail.com>
2023-09-06 12:02:52 -0400

    added register page

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const winston = require("winston");
 const bcrypt = require('bcrypt')
-const {imbd} = require('./models')
+const {Study} = require('./models')
 app.use(express.json())
-//link ejs/css
 app.use(express.static(__dirname + '/styles'));
 const path = require('path')
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get('/', (req, res) => {
+    res.render('register')
+})
+
+
+
+
+
+
 
 
 
