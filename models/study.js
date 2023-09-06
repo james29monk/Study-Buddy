4cb6139c3710894af9184434c5ce2a17f3299b52
Tony Vu <tony.vuu18@gmail.com>
2023-09-06 16:12:21 -0400

    registration confirmation text after signing up

---
+++ models/study.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Study extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Study.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    repassword: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Study',
+  });
+  return Study;
+};
