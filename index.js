e4452327b091a508a2723e5d774d0943626f8599
Tony Vu <tony.vuu18@gmail.com>
2023-09-06 14:18:33 -0400

    made our post route work

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const winston = require("winston");
 const bcrypt = require('bcrypt')
-const {Study} = require('./models')
+const { User } = require('./models')
 app.use(express.json())
 app.use(express.static(__dirname + '/styles'));
 const path = require('path')
@@ -11,14 +11,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
+
+
+
 app.get('/', (req, res) => {
     res.render('register')
 })
 
+app.post('/register', async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    try {
+        const saltRounds = 10; 
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashedPassword, // Store the hashed password
+            repassword: hashedPassword, // Store the hashed password
+        });
 
 
 
-
+        return res.render('register', { success: 'Account created successfully' });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).render('register', { error: 'Failed to create user' });
+    }
+});
 
 
 
--- migrations/20230906174256-create-user.js
+++ migrations/20230906174256-create-user.js
@@ -2,14 +2,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Studies', {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
         type: Sequelize.STRING
       },
       email: {
@@ -32,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Studies');
+    await queryInterface.dropTable('Users');
   }
 };
